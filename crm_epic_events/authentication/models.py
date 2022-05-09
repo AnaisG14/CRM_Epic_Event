be331b565a758cd7199fbdd7e213d515f4362bdf
AnaisG14 <gatard.anais@gmail.com>
2022-05-09 08:15:36 +0200

    creation of the project architecture

---
+++ crm_epic_events/authentication/models.py
@@ -0,0 +1,26 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+#
+#
+# class Category(models.Model):
+#     SAILOR = 'SAILOR'
+#     SUPPORT = 'SUPPORT'
+#     MANAGER = 'MANAGER'
+#
+#     ROLE_CHOICES = (
+#         (SAILOR, 'Sailor team'),
+#         (SUPPORT, 'Support team'),
+#         (MANAGER, 'Management team'),
+#     )
+#     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+#
+#
+# class User(AbstractUser):
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+
+
+
+
+
