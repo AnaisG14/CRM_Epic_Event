6459b1bfb0fd42225964fdff2cbcf2e63a73eb91
AnaisG14 <gatard.anais@gmail.com>
2022-06-03 21:33:34 +0200

    add LOGGING to the settings

--- crm_epic_events/settings.py
+++ crm_epic_events/settings.py
@@ -143,3 +143,21 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5,
 }
+
+LOGGING = {
+    'version': 1,                       # the dictConfig format version
+    'disable_existing_loggers': False,  # retain the default loggers
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['file'],
+        },
+    },
+}
+
